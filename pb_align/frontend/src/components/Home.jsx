e811fae19da73e2ffd9d0a158b5c94111071887e
DC <deepthi@civicdatalab.in>
2019-05-21 23:41:49 +0530

    show translation on selection

--- pb_align/frontend/src/components/Home.jsx
+++ pb_align/frontend/src/components/Home.jsx
@@ -43,42 +43,49 @@ class Home extends Component {
                     {this.props.user.username} (<a onClick={this.props.logout}>logout</a>)
                 </div>
                 <div id = "wrapper" className="d-flex">
-                <div className="bg-light border-right" id="sidebar-wrapper">
-                  <div className="list-group list-group-flush">
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
-                  </div>
-                </div>
+                    <div className="bg-light border-right" id="sidebar-wrapper">
+                      <div className="list-group list-group-flush">
+                        {this.props.notes.map((note, id) => (
+                            <a key={`note_${note.id}`} className="list-group-item list-group-item-action bg-light" onClick={() => this.selectForEdit(id)}>{note.text}</a>
+                        ))}
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
+                        <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
+                      </div>
+                    </div>
 
-                <div className="bg-light border-right" id="page-content-wrapper">
-                    <h3>Add new note</h3>
-                    <form onSubmit={this.submitNote}>
-                        <input
-                            value={this.state.text}
-                            placeholder="Enter note here..."
-                            onChange={(e) => this.setState({text: e.target.value})}
-                            required />
-                        <button onClick={this.resetForm}>Reset</button>
-                        <input type="submit" value="Save Note" />
-                    </form>
+                    <div className="bg-light border-right" id="page-content-wrapper">
+                        <h3>Add new note</h3>
+                        <form onSubmit={this.submitNote}>
+                            <input
+                                value={this.state.text}
+                                placeholder="Enter note here..."
+                                onChange={(e) => this.setState({text: e.target.value})}
+                                required />
+                            <button onClick={this.resetForm}>Reset</button>
+                            <input type="submit" value="Save Note" />
+                        </form>
 
-                    <h3>Notes</h3>
-                    <table>
-                        <tbody>
-                            {this.props.notes.map((note, id) => (
-                                <tr key={`note_${note.id}`}>
-                                    <td>{note.text}</td>
-                                    <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
-                                    <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                        <h3>Notes</h3>
+                        <table>
+                            <tbody>
+                                {this.props.notes.map((note, id) => (
+                                    <tr key={`note_${note.id}`}>
+                                        <td>{note.text}</td>
+                                        <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
+                                        <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div>
+                            <span> {this.state.text} </span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         )
